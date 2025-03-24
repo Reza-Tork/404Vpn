4dcb131810902cded2b32cb174b2da1d5e2e1261
Reza-Tork <firstprogrammer0@gmail.com>
2025-03-24 23:52:13 +0330

    Add project files.

---
+++ Bot/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var host = Host.CreateDefaultBuilder(args)
+            //.ConfigureServices((context, services) =>
+            //{
+            //    var connectionString = "Server=78.47.214.54;Database=unknown_bot;User=unknown_bot_user;Password=DFdsfddf#R$#22!1#$!!;Port=3306;";
+
+            //    //services.AddDbContext<BotDbContext>(options =>
+            //    //{
+            //    //    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+            //    //        sqlOptions =>
+            //    //        {
+            //    //            sqlOptions.EnableRetryOnFailure(20);
+            //    //            sqlOptions.MigrationsAssembly("Data");
+            //    //        });
+
+            //    //});
+
+            //    services.AddHttpClient<ITelegramBotClient, TelegramBotClient>((httpClient, sp) =>
+            //        new TelegramBotClient("7497401140:AAG0Cl20z602H0V9Uk8gmTnxrUOGtBl_BUU", httpClient)
+            //    );
+
+            //    services.AddScoped<BotService>();
+            //})
+            //.Build();
+
+            //var botService = host.Services.GetRequiredService<BotService>();
+            //await botService.Start();
+
+            //Console.WriteLine("Bot is running...");
+
+            //await Task.Delay(Timeout.Infinite);
+
+            //botService.Stop();
+        }
+    }
+}
---
+++ Domain/Entities/Bot/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Bot
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public long UserId { get; set; }
+        public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Admin? Admin { get; set; }
+        public bool IsAdmin()
+        {
+            return Admin != null;
+        }
+    }
+}
